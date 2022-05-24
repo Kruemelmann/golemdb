4f3a459978ff299109e84c26cca9b19edef5b11a
kruemelmann <kruemelmannlp@gmail.com>
2022-05-24 08:28:34 +0200

    small refactorings

--- pkg/raft/raft.go
+++ pkg/raft/raft.go
@@ -1,13 +1,13 @@
 package raft
 
-type ConsensusModule struct {
-	State StateType
-}
-
 func NewConsensusModule() *ConsensusModule {
 	return &ConsensusModule{
 		State: State.Follower,
 	}
+}
+
+type ConsensusModule struct {
+	State StateType
 }
 
 func (c *ConsensusModule) RequestVotes()  {}
